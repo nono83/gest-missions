38dac26ef36553a9bfce0f92f39ec9f575ab6033
nono83 <arnaud.gianati@club-internet.fr>
2022-10-18 11:25:05 +0200

    feat : crud entity contact
    Form :
    - Définition des types pour chaque champs dans le formulaire
    
    Template:
    - Traduction des textes
    - Changement du positionnement et de l'aspect des boutons
    - Changement du nom des blocks pour que cela match avec les noms de blocks du template base.html.twig
    - Intégration du composant Datatables dans le template index.html.twig pour l'affichage de la liste des statuts
    
    Controller :
    - changement du nom des routes
    -Ajout de l'attribut requirements pour l'id
    -Récupération de l'entité avec la méthode findOneBy du repository à la place des paramconverters

--- src/Repository/ContactRepository.php
+++ src/Repository/ContactRepository.php
@@ -39,6 +39,149 @@ class ContactRepository extends ServiceEntityRepository
         }
     }
 
+    // Get the total number of elements
+    public function countContact()
+    {
+        return $this
+            ->createQueryBuilder('contact')
+            ->select("count(contact.id)")
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    public function getRequiredDTData($start, $length, $orders, $search, $columns, $otherConditions)
+    {
+        // Create Main Query
+        $query = $this->createQueryBuilder('contact');
+        
+        // Create Count Query
+        $countQuery = $this->createQueryBuilder('contact');
+        $countQuery->select('COUNT(contact)');
+
+        // Create inner joins
+        $query
+            ->join('contact.nationalite', 'n')
+            ->join('contact.mission', 'm');
+        
+        $countQuery
+            ->join('contact.nationalite', 'n')
+            ->join('contact.mission', 'm');
+        
+        // Other conditions than the ones sent by the Ajax call ?
+        if ($otherConditions === null)
+        {
+            // No
+            // However, add a "always true" condition to keep an uniform treatment in all cases
+            $query->where("1=1");
+            $countQuery->where("1=1");
+        }
+        else
+        {
+            // Add condition
+            $query->where($otherConditions);
+            $countQuery->where($otherConditions);
+        }
+        
+        // Fields Search
+        foreach ($columns as $key => $column)
+        {
+            if ($column['search']['value'] != '')
+            {
+                // $searchItem is what we are looking for
+                $searchItem = $column['search']['value'];
+                $searchQuery = null;
+        
+                // $column['name'] is the name of the column as sent by the JS
+                switch($column['name'])
+                {
+                    case 'contact':
+                        {
+                            $searchQuery = 'contact.nom LIKE \'%'.$searchItem.'%\'';
+                            break;
+                        }
+
+                    case 'code':
+                        {
+                            $searchQuery = 'contact.code LIKE \'%'.$searchItem.'%\'';
+                            break;
+                        }
+
+                    case 'mission':
+                        {
+                            $searchQuery = 'm.titre LIKE \'%'.$searchItem.'%\'';
+                            break;
+                        }
+
+                    case 'nationalite':
+                        {
+                            $searchQuery = 'p.nom LIKE \'%'.$searchItem.'%\'';
+                            break;
+                        }
+
+                    
+                }
+        
+                if ($searchQuery !== null)
+                {
+                    $query->andWhere($searchQuery);
+                    $countQuery->andWhere($searchQuery);
+                }
+            }
+        }
+        
+        // Limit
+        $query->setFirstResult($start)->setMaxResults($length);
+        
+        // Order
+        foreach ($orders as $key => $order)
+        {
+            // $order['name'] is the name of the order column as sent by the JS
+            if ($order['name'] != '')
+            {
+                $orderColumn = null;
+            
+                switch($order['name'])
+                {
+                    case 'contact':   
+                        $orderColumn = 'contact.nom';
+                        break;
+                        
+
+                    case 'code':
+                        $orderColumn = 'contact.nom_code';
+                        break;
+                        
+
+                    case 'date_naissance':
+                        $orderColumn = 'contact.date_naissance';
+                        break;
+
+                    case 'pays':
+                        $orderColumn = 'p.nom';
+                        break;    
+
+                    case 'mission':
+                        $orderColumn = 'm.titre';
+                        break;                       
+                }
+        
+                if ($orderColumn !== null)
+                {
+                    $query->orderBy($orderColumn, $order['dir']);
+                }
+            }
+        }
+        
+        // Execute
+        $results = $query->getQuery()->getResult();
+        $countResult = $countQuery->getQuery()->getSingleScalarResult();
+        
+        return array(
+            "results" 		=> $results,
+            "countResult"	=> $countResult
+        );
+    }
+
 //    /**
 //     * @return Contact[] Returns an array of Contact objects
 //     */
